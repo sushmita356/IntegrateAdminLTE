0014bb6c3e87edfa41f7f17d31f59e1e2365fef0
sushmita <mmsushma9999@gmail.com>
2020-12-30 13:30:48 +0530

    content added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
-import Content from './components/Content';
+import React from "react";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div>
-      <Header/>
-      <Menu/>
-      <Footer/>
-      <Content/>
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
     </div>
-  )
+  );
 }
